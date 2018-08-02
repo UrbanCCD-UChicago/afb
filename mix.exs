f7c862c05ac2284e7ffe81135ecff4e312b2248d
Vince Forgione <vforgione@uchicago.edu>
2018-08-02 10:14:58 -0500

    Bumped Minor Version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Afb.Mixfile do
   def project do
     [
       app: :afb,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
